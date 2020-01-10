e405b36a9f76641f7c54d1749edc4dddb70c03c8
Hoang Phan <hoanghphan@users.noreply.github.com>
2020-01-10 09:45:33 -0800

    added const and model_def files

---
+++ model_def.py
@@ -0,0 +1,48 @@
+import collections
+import logging
+from typing import Any, Dict, Tuple
+
+import pedl
+from pedl.trial import get_gang_addrs
+
+import run_classifier
+import radam
+
+def get_tf_flags(hparams):
+
+    flags = tf.flags
+
+    tf.flags.DEFINE_string(
+        "tpu_name", None,
+        "The Cloud TPU to use for training. This should be either the name "
+        "used when creating the Cloud TPU, or a grpc://ip.address.of.tpu:8470 "
+        "url.")
+
+    tf.flags.DEFINE_string(
+        "tpu_zone", None,
+        "[Optional] GCE zone where the Cloud TPU is located in. If not "
+        "specified, we will attempt to automatically detect the GCE project from "
+        "metadata.")
+
+    tf.flags.DEFINE_string(
+        "gcp_project", None,
+        "[Optional] Project name for the Cloud TPU-enabled project. If not "
+        "specified, we will attempt to automatically detect the GCE project from "
+        "metadata.")
+
+    tf.flags.DEFINE_string("master", None, "[Optional] TensorFlow master URL.")
+
+    flags.DEFINE_integer(
+        "num_tpu_cores", 8,
+        "Only used if `use_tpu` is True. Total number of TPU cores to use.")
+
+    combined_dict = hparams + pedl.get_data_config()
+    for key in combined_dict:
+        FLAGS[key].value = combined_dict[key]
+
+    return flags
+
+
+if __name__ == "__main__":
+
+    flags = get_tf_flags()
